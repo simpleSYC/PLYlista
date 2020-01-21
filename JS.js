b1a9ac3a2e16aa965d88370e6c4666cfcdbbe181
simpleSYC <57138908+simpleSYC@users.noreply.github.com>
2020-01-21 14:17:57 +0100

    Update JS.js

--- JS.js
+++ JS.js
@@ -189,6 +189,18 @@ document.getElementById("akcijaTM").style.height =screen.height+"px";
 ////////////////(document.getElementById("1pv1")).play();
 }
 
+///////////////
+function ONLOADedDATA(){
+	
+	
+}
+
+
+////////////////////
+
+
+
+
 function CloseFiniSOT() {
    document.getElementById("myModalTm").style.display = "none";
   
@@ -288,7 +300,7 @@ var Tn_1_TXTurl="./WKUP.ogg";
 var Tn_2_TXTurl="./sounds/vlc-record-2019-09-26-12h28m33s-Drone Quadrocopter Flying Sound Effect [High Quality]-.mp4";
 var Tn_3_TXTurl="https://www.youtube.com/watch?v=Y5M6YKR7wUw";
 var Tn_4_TXTurl="https://www.youtube.com/watch?v=";
-var Tn_5_TXTurl="https://www.youtube.com/watch?v=5555555";
+var Tn_5_TXTurl="https://content.production.cdn.art19.com/validation=1579689711,7a441930-62d7-5ad2-be3f-4e5dff68711b,e6H_Dia53mE8SdRo_ZMs5oehn6A/episodes/8ad8c3ec-0c64-459b-b1cc-7ef8e1125acb/b47126484c836b47ace80540c0c98006b755d50fad2fdfcda6cbc4e2ad621da31ef1ea2dfe33aedc9d36db20d8a9b0b23d8f07843f8be9f3f715b8695b381c6e/JP%20EP%2042%20011920%20Mixdown%201.mp3";
 var Tn_6_TXTurl="https://www.youtube.com/watch?v=666666666";
 var Tn_7_TXTurl="https://www.youtube.com/watch?v=";
 var Tn_8_TXTurl="https://www.youtube.com/watch?v=";

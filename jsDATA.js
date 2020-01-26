d45376ae1d848e9686c117eff43aac30b488a94a
simpleSYC <57138908+simpleSYC@users.noreply.github.com>
2020-01-26 11:08:59 +0100

    Add files via upload

---
+++ jsDATA.js
@@ -0,0 +1,106 @@
+
+
+ /// uste samo sedmicata da se dodade!!!
+/// ????   JP%20EP%204B%20Akira%20the%20Don%20041419%20Mastered%20Final.mp3?alt=media&token=a168b5d6-27e0-41d0-a556-f6ca50b8b438
+
+/// jp lirving    JP%20Irving%20092919.mp3?alt=media&token=df926762-625b-482a-9864-9b9c0aa8a569
+
+/// 1  JP%20EP%201C.mp3?alt=media&token=44ae0cbf-6a29-45cd-bc73-26fba59df524
+
+/// 3  JP%20EP%203%20040719%20Portland.mp3?alt=media&token=7cabc780-5b2a-4c7c-b420-3f17dbb3847d
+ 
+ /// 5  JP%20EP%205%20Sacramento%20042119.mp3?alt=media&token=4020d65b-0c79-4125-8434-1bce6d4cb00c
+ 
+ /// 6  JP%20EP%206%20Thousand%20Oaks%20042819.mp3?alt=media&token=c72a1e07-409b-4385-865c-86552cca8499
+ 
+/// 8  JP%20EP%208%20051219B%20Hamilton%20Ontario.mp3?alt=media&token=5a15da01-ddbc-487a-b0c7-b41971211972
+
+/// 13  JP%20EP%2013%20061619%20Kitchener%20%20Mixdown%202.mp3?alt=media&token=b9a93b77-6ddf-4712-b017-9e52aa7b3ebe
+
+/// 10  JP%20Ep%2010%20052619%20Vancouver.mp3?alt=media&token=4a723175-b45e-4a5d-a5e3-e4f38d442948
+
+/// 23  JP%20Ep%2023%20082519%20072818%20Edmonton%20AB%20Mixdown%201.mp3?alt=media&token=ed565f4b-2be1-4ab2-a5df-da61ca997b3c
+
+/// 24  JP%20Ep%2024%20Winnipeg%20MA%20090119.mp3?alt=media&token=06450c97-cd6f-48c5-99aa-986150d0d1b1
+
+// 26  26_-_The_Phenomenology_of_the_Divine.m4a?alt=media&token=6c55ee77-a75b-4776-8fcf-2b3b41dfbbe6
+
+///  27  JP%20EP%2027%20Westbury%20092219B.mp3?alt=media&token=59179ab2-8df3-4655-aff8-7ae885a898f5
+
+/// 29  JP%20EP%2029%20100619_01revised.mp3?alt=media&token=739dd50a-5adf-4f89-823f-08105934df63
+
+/// 30  JP%20EP%2030%20101319.mp3?alt=media&token=91969d65-e41c-461c-888f-93c9a91186d4
+
+/// 32  JP%20EP%2032%20102719%20%20The%20Topic%20of%20Truth.mp3?alt=media&token=720831b7-9520-4c4e-bc46-b0a08469190b
+
+/// 33  JP%20Ep%2033%20Cambridge%20UK%20110319.mp3?alt=media&token=e1f9f5ff-a9f6-459c-9815-b2abd72efd03
+
+/// 34  JP%20EP%2034%20111019%20Birmingham%20UK.mp3?alt=media&token=ad76b787-34be-46a7-91fb-f01ae3acae4c
+
+/// 36  JP%20EP%2036%20112419%20%20Value%20is%20bound%20by%20Limitation.mp3?alt=media&token=008da4ad-75fa-417a-9cd3-889edffeefad
+
+/// 35  JP%20EP%2035%20111719.mp3?alt=media&token=d725792f-ac91-486c-9232-6006b299e7af
+
+/// 37  JP%20EP%2037%20120119%20The%20Struggle%20Between%20Chaos%20and%20Order_01.mp3?alt=media&token=000df125-0854-4056-b0ed-8128c8a75618
+
+// 38  38_-_Cathy_Newman_and_analysis_with_Geenstijl.m4a?alt=media&token=87dd942b-1a3c-47bd-a0de-67d29700683f
+
+/// 39  JP%20EP%2039%20121519%20012419%20Riverside%20%20%20Mixdown%201.mp3?alt=media&token=8e31dd9e-c12c-41e0-9aed-53a08dd0dbf0
+
+/// 40  JP%20Ep%2040%20Beyond%20Order%20012519%20122219%20Another%2012%20Rules%20for%20Life.mp3?alt=media&token=35ebe10c-4df9-4846-89b7-0bf12f423959
+
+/// 41  JP%20Ep%2041%20Defense%20Against%20ideological%20Possession.mp3?alt=media&token=31247454-e279-4fb6-901e-5d42e4284830
+
+/// 42  JP%20EP%2042%20011920%20Mixdown%201.mp3?alt=media&token=7a92ad91-bb4c-4664-9cc7-de9e373fb80c
+
+
+var MYurl="https://firebasestorage.googleapis.com/v0/b/test01-2867c.appspot.com/o/";
+
+var Tn_0_TXTurl="./WKUP.ogg"; ///////////!!!
+ 
+var Tn_1_TXTurl="JP%20EP%201C.mp3?alt=media&token=44ae0cbf-6a29-45cd-bc73-26fba59df524";
+
+var Tn_2_TXTurl="JP%20Irving%20092919.mp3?alt=media&token=df926762-625b-482a-9864-9b9c0aa8a569";
+ 
+var Tn_3_TXTurl="JP%20EP%203%20040719%20Portland.mp3?alt=media&token=7cabc780-5b2a-4c7c-b420-3f17dbb3847d";
+ 
+var Tn_4_TXTurl="JP%20EP%205%20Sacramento%20042119.mp3?alt=media&token=4020d65b-0c79-4125-8434-1bce6d4cb00c";
+ 
+var Tn_5_TXTurl="";
+ 
+var Tn_6_TXTurl="";
+ 
+var Tn_7_TXTurl="";
+ 
+var Tn_8_TXTurl="";
+ 
+var Tn_9_TXTurl="";
+ 
+var Tn_10_TXTurl="";
+ 
+var Tn_11_TXTurl="";
+ 
+var Tn_12_TXTurl="";
+ 
+var Tn_13_TXTurl="";
+ 
+var Tn_14_TXTurl="";
+ 
+var Tn_15_TXTurl="";
+ 
+var Tn_16_TXTurl="";
+ 
+var Tn_17_TXTurl="";
+ 
+var Tn_18_TXTurl="";
+ 
+var Tn_19_TXTurl="";
+ 
+var Tn_20_TXTurl="";
+ 
+
+
+
+
+
+
